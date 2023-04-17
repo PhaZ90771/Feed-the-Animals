90f3cda7c39b84cae4a98dd6890ea1fc0c52a2d4
Benjamin Shutt <shuttben@gmail.com>
2023-04-16 23:32:46 -0500

    Final score is now displayed in the game over scene. ScoreTracker is destroyed if you load the main menu. Also fixed error caused by ScoreTracker component that was missing the the associated container game object.

--- Assets/Scripts/LoadScene.cs
+++ Assets/Scripts/LoadScene.cs
@@ -7,6 +7,11 @@ public class LoadScene : MonoBehaviour
 {
     public void LoadSceneByIndex(int index)
     {
+        if (index == 0) // Destroy the score tracker if going to the main menu
+        {
+            var scoreTracker = FindAnyObjectByType<ScoreTracker>();
+            if (scoreTracker) Destroy(scoreTracker);
+        }
         SceneManager.LoadScene(index);
     }
 }
